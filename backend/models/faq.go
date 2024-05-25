0f5a9274a3988f82d9d2fbd2b60210a046b14489
HooEP01 <hooernping02@gmail.com>
2024-05-25 12:07:21 +0800

    improve faq model

--- backend/models/faq.go
+++ backend/models/faq.go
@@ -9,7 +9,9 @@ import (
 type Faq struct {
 	gorm.Model
 	ID        uint           `gorm:"primarykey"`
+	TopID     uint           `json:"top_id"`
 	ParentID  uint           `json:"parent_id"`
+	ParentIDs string         `json:"parent_ids"`
 	FaqTypeID uint           `json:"faq_type_id"`
 	FaqType   FaqType        `json:"faq_type" gorm:"references:ID"`
 	Answer    string         `json:"answer"`
