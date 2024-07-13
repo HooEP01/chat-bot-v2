76ef353405d9d08643a23852146cc58750b224a0
HooEP01 <hooernping02@gmail.com>
2024-07-13 19:49:20 +0800

    layout developed

--- frontend/src/components/Auth/Auth.tsx
+++ frontend/src/components/Auth/Auth.tsx
@@ -3,7 +3,7 @@ import { AppDispatch } from "../../store";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../store/auth/authSlice";
 import { LoginForm } from "../../model/user.model";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { getStatus, getToken } from "../../selectors/auth";
 import { StateStatus } from "../../model/state.model";
 import { useEffect } from "react";
@@ -30,53 +30,64 @@ const Auth = () => {
     if (token && status === StateStatus.Succeeded) {
       navigate("/faq");
     }
-  }, [token, status, navigate])
+  }, [token, status, navigate]);
 
   return (
-    <div className="card m-6 md:w-96 bg-base-100 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">Welcome to Chat Bot v2</h2>
-        <p className="text-start mb-3">
-          One of the best, best of the best chat bot in the world.
-        </p>
+    <div className="max-w-[24rem] mx-auto">
+      <div className="card px-4 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <p className="card-title block text-2xl text-center">Welcome back.</p>
+          <p className="text-center mb-4">
+            New to us? {""}
+            <a className="underline font-semibold" href="/register">
+              Sign Up
+            </a>
+          </p>
 
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-col gap-4">
-            {/* email field */}
-            <div className="flex flex-col gap-2">
-              <label htmlFor="email" className="text-sm font-medium">
-                Email
-              </label>
-              <label className="input input-bordered flex items-center gap-2">
-                <input
-                  {...register("email", { required: true })}
-                  type="text"
-                  className="grow"
-                />
-              </label>
-            </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-4">
+              {/* email field */}
+              <div className="flex flex-col gap-2">
+                <label htmlFor="email" className="text-sm font-medium">
+                  Your email
+                </label>
+                <label className="input input-bordered flex items-center gap-2">
+                  <input
+                    {...register("email", { required: true })}
+                    type="text"
+                    className="grow"
+                  />
+                </label>
+              </div>
 
-            {/* password field */}
-            <div className="flex flex-col gap-2">
-              <label htmlFor="password" className="text-sm font-medium">
-                Password
-              </label>
-              <label className="input input-bordered flex items-center gap-2">
-                <input
-                  {...register("password", { required: true })}
-                  type="password"
-                  className="grow"
-                />
-              </label>
-            </div>
+              {/* password field */}
+              <div className="flex flex-col gap-2">
+                <label htmlFor="password" className="text-sm font-medium">
+                  Your password
+                </label>
+                <label className="input input-bordered flex items-center gap-2">
+                  <input
+                    {...register("password", { required: true })}
+                    type="password"
+                    className="grow"
+                  />
+                </label>
+              </div>
 
-            <div className="card-actions w-full mt-2">
-              <button type="submit" className="btn btn-primary btn-block">
-                LOGIN
-              </button>
+              <div className="card-actions w-full mt-2">
+                <button type="submit" className="btn btn-primary btn-block">
+                  LOGIN
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+
+          <p className="mb-4">
+            <a className="underline font-semibold" href="/faq">
+              Trouble logging in?
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -4,7 +4,13 @@ import Loading from "../components/Loading";
 import { motion } from "framer-motion";
 import CustomToast from "../components/CustomToast";
 import CustomIcon from "../components/CustomIcon";
-import { IconMenu } from "@tabler/icons-react";
+import {
+  IconBell,
+  IconMenu,
+  IconMessage,
+  IconUser,
+  IconZoomQuestion,
+} from "@tabler/icons-react";
 
 const Layout = () => {
   const location = useLocation();
@@ -33,9 +39,8 @@ const Layout = () => {
           />
 
           <div className="drawer-content bg-base-100">
-
-             {/* Header */}
-             <div className="navbar w-full border md:hidden">
+            {/* Header */}
+            <div className="navbar w-full md:hidden">
               {/* <!-- Logo --> */}
 
               <div className="flex justify-between w-full">
@@ -43,70 +48,85 @@ const Layout = () => {
                   <button className="btn btn-ghost text-xl">Chat Bot</button>
                 </div>
 
-                <label htmlFor="my-drawer" className="btn drawer-button lg:hidden">
+                <label
+                  htmlFor="my-drawer"
+                  className="btn drawer-button lg:hidden"
+                >
                   <CustomIcon icon={IconMenu} />
                 </label>
               </div>
             </div>
             {/* main */}
-            <div className="content-wrapper transition-all duration-150 p-6">
+            <div className="content-wrapper transition-all duration-150">
               {/* md:min-h-screen will h-full*/}
               <div className="page-content page-min-height">
-                <div className="container mx-auto">
-                  <Suspense fallback={<Loading />}>
-                    <motion.div
-                      key={location.pathname}
-                      initial="pageInitial"
-                      animate="pageAnimate"
-                      exit="pageExit"
-                      variants={{
-                        pageInitial: {
-                          opacity: 0,
-                          y: 50,
-                        },
-                        pageAnimate: {
-                          opacity: 1,
-                          y: 0,
-                        },
-                        pageExit: {
-                          opacity: 0,
-                          y: -50,
-                        },
-                      }}
-                      transition={{
-                        type: "tween",
-                        ease: "easeInOut",
-                        duration: 0.5,
-                      }}
-                    >
-                      {/* <Breadcrumbs /> */}
-                      {<Outlet />}
-                    </motion.div>
-                  </Suspense>
+                <div className="container max-w-screen-lg mx-auto">
+
+                  <div className="flex h-28 w-full justify-end items-center gap-4 px-4 md:px-8">
+                    <button className="btn btn-circle">
+                      <CustomIcon icon={IconBell} />
+                    </button>
+                    <button className="btn btn-circle">
+                      <CustomIcon icon={IconUser} />
+                    </button>
+                  </div>
+
+                  <div className="p-4 md:p-8">
+                    <Suspense fallback={<Loading />}>
+                      <motion.div
+                        key={location.pathname}
+                        initial="pageInitial"
+                        animate="pageAnimate"
+                        exit="pageExit"
+                        variants={{
+                          pageInitial: {
+                            opacity: 0,
+                            y: 50,
+                          },
+                          pageAnimate: {
+                            opacity: 1,
+                            y: 0,
+                          },
+                          pageExit: {
+                            opacity: 0,
+                            y: -50,
+                          },
+                        }}
+                        transition={{
+                          type: "tween",
+                          ease: "easeInOut",
+                          duration: 0.5,
+                        }}
+                      >
+                        {/* <Breadcrumbs /> */}
+                        {<Outlet />}
+                      </motion.div>
+                    </Suspense>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
 
           {/* side drawer */}
-          <div className="drawer-side border">
+          <div className="drawer-side">
             <label
               htmlFor="my-drawer"
               aria-label="close sidebar"
               className="drawer-overlay"
             ></label>
 
-            <div className="menu p-4 w-64 min-h-full bg-base-200 text-base-content">
-              <ul className="menu bg-base-200 w-56 rounded-box">
-                <li className="mb-4">
-                  <button className="btn btn-ghost text-xl">Chat Bot</button>
-                </li>
-
+            <div className="menu p-4 w-64 min-h-full text-base-content bg-base-200 md:bg-transparent">
+              <div className="flex justify-center items-center h-28">
+                <p className="text-xl font-bold text-center">CHATAPP</p>
+              </div>
+              <ul className="menu w-56 rounded-box gap-4">
                 <li>
                   <Link
                     to="/faq"
                     className={currentPath === "/faq" ? "active" : ""}
                   >
+                    <CustomIcon icon={IconZoomQuestion} />
                     Faq
                   </Link>
                 </li>
@@ -115,6 +135,7 @@ const Layout = () => {
                     to="/chat"
                     className={currentPath === "/chat" ? "active" : ""}
                   >
+                    <CustomIcon icon={IconMessage} />
                     Chat
                   </Link>
                 </li>
